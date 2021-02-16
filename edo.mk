7c4a8c9bad8e0b23a9af6b7d28089e9b5f0212db
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    sm8250-common: Migrate to Sony power HAL AIDL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- edo.mk
+++ edo.mk
@@ -425,9 +425,21 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.2.vendor \
+    android.hardware.power-service.sony-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+# sendhint utility
+PRODUCT_PACKAGES += \
+    sendhint
+
+# Enable adpf cpu hint session for SurfaceFlinger and HWUI
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_adpf_cpu_hint=true \
+    debug.hwui.use_hint_manager=true \
+    debug.hwui.target_cpu_time_percent=30
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -457,7 +469,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/sony
+    hardware/sony \
+    hardware/google/pixel \
+    hardware/google/interfaces
 
 # Shim
 PRODUCT_PACKAGES += \
