dc6aee1108be5a3a5d4aeeca96d0fba342bdb8a6
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 16:25:29 -0700

    sm8250-common: Drop PRODUCT_BUILD_SUPER_PARTITION
     * Super image is not generated by a default 'm' build,
       but we at least want it for dist builds.
    
    Change-Id: Ia12a5314632509fcd309d51ddf757efdc37499c3

--- edo.mk
+++ edo.mk
@@ -441,5 +441,4 @@ $(call inherit-product-if-exists, hardware/sony/XperiaModules.mk)
 # Keys for signing
 -include vendor/ahnet-priv/keys/keys.mk
 
-PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
