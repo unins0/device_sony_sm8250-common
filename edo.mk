9c04cfa1339acb9d3a0afcb4e4673f57c188d902
John Galt <johngaltfirstrun@gmail.com>
2022-05-23 10:37:45 -0400

    sm8250-common: edo.mk: make dex2oat go fast
    Make ota updater go a bit faster.

--- edo.mk
+++ edo.mk
@@ -535,6 +535,13 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
+# Dex to make OTA go faster
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    dalvik.vm.boot-dex2oat-cpu-set=1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=7 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
+
 # Xperia Modules
 $(call inherit-product, hardware/sony/XperiaModules.mk)
 
