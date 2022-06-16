ccc6991eeca7a7df302bb269dd677132dadb59d9
Cyber Knight <cyberknight755@gmail.com>
2022-06-17 02:12:27 +0800

    sm8250-common: Build KProfiles
    - Inherit KProfiles.
    - Address sepolicy.
    - Define sysfs_kprofiles as a domain.
    - Fix permission of nodes relevant to KProfiles at boot.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- common.mk
+++ common.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light-V1-ndk_platform.vendor \
