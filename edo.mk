c902a20cef4d6653d8f40e967d9cb7b7d0fb313d
Roman Kiryanov <rkir@google.com>
2022-08-30 17:18:22 -0700

    sm8250-common: Switch sensors multihal to AIDL
    Bug: 205154779
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Change-Id: I13c7bb690f3d66e164d0caed0af6bfcf6769ce91

--- edo.mk
+++ edo.mk
@@ -470,7 +470,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors-service.multihal \
     libsensorndkbridge
 
 PRODUCT_SOONG_NAMESPACES += \
