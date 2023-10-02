b82b0b9af126480c5140346fbd5e7058c8c99a51
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-10-03 02:10:31 +0530

    sm8250-common: Stop copying fstab.qcom twice
     * We already do that through rootdir/Android.mk
    
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- edo.mk
+++ edo.mk
@@ -301,10 +301,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fstab
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:vendor/etc/fstab.qcom
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
@@ -459,10 +455,6 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
-
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
 # RIL
 PRODUCT_PACKAGES += \
