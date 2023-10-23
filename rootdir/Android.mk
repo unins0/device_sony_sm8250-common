588bfff1ba857480bbc7c18737e32f52f5b93bc1
Mashopy <eliasgheeraert@gmail.com>
2023-10-23 23:55:53 +0530

    sm8250-common: Rename init.qcom.msim.sh to init.sony.sh
     * We use it for more than setting multisim prop
    
    Change-Id: I199363cee7b828bee661d6398fb35107438b41d4

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -238,10 +238,10 @@ LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.msim.sh
+LOCAL_MODULE       := init.sony.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qcom.msim.sh
+LOCAL_SRC_FILES    := bin/init.sony.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
