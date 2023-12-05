6d400aa62cf99c49b7c556bf759ac8c517b27ee6
Jake Weinstein <jake@aospa.co>
2023-12-06 04:46:13 +0530

    sm8250-common: Do not balance msm_drm and kgsl_3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    And also affine them.
    
    Change-Id: If49ecb8757d133a7fad0d7946837b35403e57c2a

--- extract-files.py
+++ extract-files.py
@@ -111,6 +111,10 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libsomc_chokoballcmn.so',
     ): blob_fixup()
         .replace_needed('libui.so', 'libui-v34.so'),
+    (
+        'vendor/etc/msm_irqbalance.conf',
+    ): blob_fixup()
+        .regex_replace('IGNORED_IRQ=27,23,38$', 'IGNORED_IRQ=27,23,38,115,332'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
