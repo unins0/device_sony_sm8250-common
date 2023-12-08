6beb8f7be6889ee842605b4afaee5890c3bf4aa8
guaiyihu <guaiyihu@foxmail.com>
2023-12-08 08:33:31 +0530

    sm8250-common: rootdir: Clean up unused init scripts
     * init.qcom.class_core.sh
     * init.qcom.coex.sh
     * init.qcom.efs.sync.sh
     * init.qcom.sdio.sh
     * qca6234-service.sh
    
    Change-Id: Ib6ba59acb1f1d6b773aeca3c326944049784887c
    (cherry picked from commit 8c8c68847316d8f83f9d69ae06a6d5157a7d7bbf)

--- edo.mk
+++ edo.mk
@@ -191,15 +191,11 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.crda.sh \
     init.mdm.sh \
-    init.qcom.class_core.sh \
-    init.qcom.coex.sh \
     init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
     init.qcom.msim.sh \
     init.qcom.factory.rc \
     init.qcom.post_boot.sh \
     init.qcom.rc \
-    init.qcom.sdio.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.rc \
@@ -209,7 +205,6 @@ PRODUCT_PACKAGES += \
     init.qti.dcvs.sh \
     init.qti.media.sh \
     init.qti.qcv.sh \
-    qca6234-service.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
