a040bcb30542b6e10eda703b90e810fd3f27c795
ahnet-69 <syedmusicrealise@gmail.com>
2024-02-04 21:31:25 +0500

    sm8250-common: Create dummy libqti-perfd-client

--- edo.mk
+++ edo.mk
@@ -425,7 +425,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor \
+    libqti-perfd-client \
     android.hardware.power-service.sony-libperfmgr
 
 PRODUCT_COPY_FILES += \
