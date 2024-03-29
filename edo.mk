e6501633c7600f227c624ec0ce73ec8ab919128d
New Author Name <adarshgrewal@gmail.com>
2024-03-29 19:53:10 +0530

    sm8250-common: Switch to AOSP WFD
    * Drop QCOM WFD blobs
    * Lets use aosp wfd which actually works

--- edo.mk
+++ edo.mk
@@ -551,15 +551,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    android.media.audio.common.types-V2-cpp \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
 
