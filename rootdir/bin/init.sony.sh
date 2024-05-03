f094f1bae270386115181615316b97a17ba33d96
ahnet-69 <syedmusicrealise@gmail.com>
2024-05-03 15:59:51 +0000

    sm8250-common: rootdir: Fixup setting type-f for japense models
    The old impl only focused on global japense models. However Carrier models also face the same issue and require such a solution.
    
    Fixes:
    libnfc_nci: unknown opcode:0x21'
    
    Test: Take a logcat and "libnfc_nci: unknown opcode:0x21'" error does not occur, Nfc works as expected
    Tested by: Nezhdan (on xda)

--- rootdir/bin/init.sony.sh
+++ rootdir/bin/init.sony.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
-
-model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
+ltapath="/dev/block/bootdevice/by-name/LTALabel"
+model=`grep -aim1 'model:' "$ltapath" | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
 case "$model" in
     "XQ-AT42" | "XQ-AT52" | "XQ-AT72" | "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
@@ -10,7 +10,41 @@ case "$model" in
 esac
 
 if [ "$model" == "" ]; then
-    setprop vendor.radio.ltalabel.model "unknown"
+
+typef="false"
+
+    # Check if they are japense felica models
+    if grep -aim1 "XQ-AS42" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop vendor.radio.ltalabel.model "XQ-AS42"
+    fi
+    if grep -aim1 "XQ-AT42" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop vendor.radio.ltalabel.model "XQ-AT42"
+    fi
+    if grep -aim1 "SOG02" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop vendor.radio.ltalabel.model "SOG02"
+    fi
+    if grep -aim1 "SOG01" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop vendor.radio.ltalabel.model "SOG01"
+    fi
+    if grep -aim1 "A002SO" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop vendor.radio.ltalabel.model "A002SO"
+    fi
+    if grep -aim1 "SO-52A" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop vendor.radio.ltalabel.model "SO-52A"
+    fi
+    if grep -aim1 "SO-51A" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop vendor.radio.ltalabel.model "SO-51A"
+    fi
+    if [ ! "$typef" == "true" ]; then
+        setprop vendor.radio.ltalabel.model "unknown"
+    fi
 else
     setprop vendor.radio.ltalabel.model "$model"
 fi
