7a899cfdc6f7639c233a5210b89c6dbf99a61184
ahnet-69 <syedmusicrealise@gmail.com>
2024-05-30 18:37:25 +0500

    sm8250-common: patches: Add patches for X-Reality engine from RisingOS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,9 +8,22 @@ echo -e
 cd frameworks/av
 git reset --hard FETCH_HEAD
 echo -e
-git am ../../device/sony/sm8250-common/patches/frameworks_av/0001-Fixes-vtservice-cpu-hogging.patch
+git am ../../device/sony/sm8250-common/patches/frameworks_av/*
 cd ../..
 echo -e
+echo 'Applying X-Reality patches in frameworks/base'
+cd frameworks/base
+git reset --hard FETCH_HEAD
+echo -e
+git am ../../device/sony/sm8250-common/patches/frameworks_base/*
+cd ../..
+echo -e
+echo 'Applying X-Reality patches in packages/apps/Settings'
+cd packages/apps/Settings
+git reset --hard FETCH_HEAD
+echo -e
+git am ../../../device/sony/sm8250-common/patches/packages_apps_Settings/*
+cd ../../..
 echo 'Done applying'
 echo -e
 echo 'Continue'
