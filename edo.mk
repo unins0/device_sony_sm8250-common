371ad941e1de76d88ee7fe4d85d4ceb94c6ce859
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    sm8250-common: Build AOSP Radio HIDL->AIDL Wrapper
    Depends-On: I790728a9be289042e5a07589934a2dd50463a507
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- edo.mk
+++ edo.mk
@@ -427,6 +427,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
