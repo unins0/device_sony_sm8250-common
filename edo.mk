052ba27f98682938b6714d228c3bad2b1435f08a
ahnet-69 <syedmusicrealise@gmail.com>
2024-06-29 10:12:40 +0000

    sm8250-common: Switch to common libqti-perfd-client

--- edo.mk
+++ edo.mk
@@ -484,6 +484,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/sony \
     hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/google/pixel \
     hardware/google/interfaces
 
