cd0d9439acd82245dd1c7039af882d442cfc3e4b
ahnet-69 <syedmusicrealise@gmail.com>
2024-08-28 18:57:02 +0000

    pdx206: Introduce patches
    * This includes necessary patches to get audio working on our device.
    * Include a auto applying patch script from Project Elixir's GSI device tree

---
+++ vendorsetup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+TOP=$(pwd)
+PATCHES=$TOP/device/sony/pdx206/patches
+RESET=true
+
+function patch () {
+    cd $TOP/$2
+
+    if $RESET; then 
+        git reset --hard FETCH_HEAD
+    fi
+
+    git config --local user.name "genericuser"
+    git config --local user.email "generic@email.com"
+    git am $FOLDER/*
+    if [ $? -ne 0 ]; then echo "!!! WARNING: Patching failed."; fi
+    git config --local --unset user.name
+    git config --local --unset user.email
+}
+
+function apply () {
+    for FOLDER in $PATCHES/*; do
+        PATCHDIR=$(basename "$FOLDER") # Remove additional path from DIR name
+
+        SOURCEPATH=${PATCHDIR/platform_/} # Remove platform_ from dir name
+        SOURCEPATH=${SOURCEPATH//_//} # replace _ with / to make a path to directory to patch
+
+	if [ $SOURCEPATH == "build" ]; then SOURCEPATH="build/make"; fi # Replace build with build/make
+
+        patch $FOLDER $SOURCEPATH
+    done
+
+    cd $TOP
+    RESET=false
+}
+
+apply
+
+echo 'Deleted vendor/qcom/opensource/commonsys/fm to avoid stupid error'
+rm -rf vendor/qcom/opensource/commonsys/fm
