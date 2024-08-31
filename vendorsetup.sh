4e31c30f44a37c3a457a1b0a679459c46f94f8bb
ahnet-69 <syedmusicrealise@gmail.com>
2024-08-31 22:00:06 +0500

    sm8250-common: vendorsetup: Add choices
    Merged @saku-bruh's vendorsetup logic with current patch applying script
    source: https://github.com/XperiaLabs/device_sony_pdx215-clo/blob/uvite/vendorsetup.sh
    
    * Applying patches every time causes it to recompile stuff which increases build time for dirty builds.
    * Also add spaces to make reading the output easier

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,7 @@ function patch () {
 
     if $RESET; then 
         git reset --hard FETCH_HEAD
+        echo -e
     fi
 
     git config --local user.name "genericuser"
@@ -28,14 +29,61 @@ function apply () {
 
 	if [ $SOURCEPATH == "build" ]; then SOURCEPATH="build/make"; fi # Replace build with build/make
 
+        echo -e
+        echo "Applying patches in ${SOURCEPATH}"
+        echo -e
+
         patch $FOLDER $SOURCEPATH
+        echo -e
     done
 
     cd $TOP
     RESET=false
 }
 
-apply
+echo "This script will apply patches and fixes to your ROMs source code."
+echo "Choose an option:"
+echo "1. Apply first build patches and start building (For a first time build or if you have repo synced to update the ROM)"
+echo "2. Skip patching process and start building (After the first build, if patches have been already been applied)"
+read -p "Enter your choice: " choice
 
-echo 'Deleted vendor/qcom/opensource/commonsys/fm to avoid stupid error'
-rm -rf vendor/qcom/opensource/commonsys/fm
+case $choice in
+    1)
+        echo -e
+        echo 'Applying patches...'
+        echo -e
+        apply
+        echo -e
+        echo 'Done Applying all Patches!'
+        echo -e
+        echo 'Checking if problematic FM folder exists...'
+        echo -e
+        if [[ -d vendor/qcom/opensource/commonsys/fm ]]; then
+            echo 'Problematic FM folder found! Removing...'
+            rm -rf vendor/qcom/opensource/commonsys/fm/
+            echo 'Problematic FM successfully removed!'
+            echo -e
+        else
+            echo 'Problematic FM folder not found, skipping removal.'
+            echo -e
+        fi
+        echo 'All patches and fixes have been applied! Starting build process...'
+        echo -e
+        ;;
+    2)
+        echo "Patches and fixes skipped... Starting build process."
+        if [[ -d vendor/qcom/opensource/commonsys/fm ]]; then
+            echo 'Problematic FM folder found! Removing...'
+            rm -rf vendor/qcom/opensource/commonsys/fm/
+            echo 'Problematic FM successfully removed!'
+            echo -e
+        else
+            echo 'Problematic FM folder not found, skipping removal.'
+            echo -e
+        fi
+        ;;
+    *)
+        echo "Invalid choice! Using option 2 as a fallback."
+        choice=2
+        ;;
+esac
