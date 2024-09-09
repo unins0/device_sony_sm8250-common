97bff6d730d8c46c862570dc578fdf8fd037b234
ahnet-69 <syedmusicrealise@gmail.com>
2024-09-09 16:54:36 +0500

    sm8250-common: Fixup of QTI alarm component
    * For some reason the hidl poweroff alarm service is disabled by aospa alarm component
    * Manually enabling it seems to work fine
    * Hence override the init rc file from dt so the service starts properly
    * Keep poweroffalarm startup in init.target.rc despite of AOSPA having declared it (it doesnt start without it)

--- common.mk
+++ common.mk
@@ -231,6 +231,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# PowerOffAlarm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/alarm/vendor.qti.hardware.alarm@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.alarm@1.0-service.rc
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
