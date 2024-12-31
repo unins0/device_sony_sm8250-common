b7ed45780b549ecf6a1209df713bcf56c3ebbc2e
unins0 <164762060+unins0@users.noreply.github.com>
2024-12-31 14:53:01 +0300

    Build android.hardware.audio.sounddose-vendor-impl.so
    E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- edo.mk
+++ edo.mk
@@ -126,6 +126,7 @@ PRODUCT_PACKAGES += \
     libaudioroute.vendor \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl \
     tinyplay \
