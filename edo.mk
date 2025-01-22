1f19f88e62e1b840a4bb8cd90e0e56d7d96cb438
John Galt <johngaltfirstrun@gmail.com>
2025-01-22 09:07:16 -0500

    sm8250-common: Wfd: move to qcom common

--- edo.mk
+++ edo.mk
@@ -439,7 +439,8 @@ PRODUCT_PACKAGES += \
 TARGET_BOARD_PLATFORM := kona
 TARGET_COMMON_QTI_COMPONENTS := \
     alarm \
-    av
+    av \
+    wfd
 
 # RIL
 PRODUCT_PACKAGES += \
@@ -523,11 +524,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.media.audio.common.types-V2-cpp \
     libnl \
-    libpng.vendor \
     libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
