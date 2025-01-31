f5d77eabd1167d9bad4d717abfbfdef0dca1f8ed
ahnet-69 <syedmusicrealise@gmail.com>
2025-01-31 15:05:53 +0800

    sm8250-common: proprietary: Nuke dup av libs
    * Add av path to soong imports

--- extract-files.py
+++ extract-files.py
@@ -26,6 +26,7 @@ namespace_imports = [
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
+    'vendor/qcom/common/system/av',
 ]
 
 
