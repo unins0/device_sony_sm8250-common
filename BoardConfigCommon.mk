4ed1b452dff1364213b0c905d6277052eff6b631
ahnet-69 <syedmusicrealise@gmail.com>
2025-03-15 15:25:38 +0500

    sm8250-common: Update extra configuration
    * We now properly inherit a BoardConfig which sets proper flags in proper places
    * Remove stamina. it has been dropped from extra
    * Reapply "sm8250-common: Remove smartstamina commands"
      -This reverts commit eab906c2ef59668c97117e0926440df4f0fd0848.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -225,3 +225,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 include vendor/sony/sm8250-common/BoardConfigVendor.mk
+include vendor/sony/extra/BoardConfigExtra.mk
