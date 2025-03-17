6d45f829049f38c4f95ee4576fbc11281bd21188
ahnet-69 <syedmusicrealise@gmail.com>
2025-03-17 21:26:11 +0500

    Revert "sm8250-common: Switch to Pixel libperfmgr"
    This reverts commit a7429954255f442f6f26e07b21d5aef10081c2c7.
    
    * Keep DT2W Nodes handling inside powerhint

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,6 +197,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-10-01
 
 # Sepolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/sony/sepolicy/qti/SEPolicy.mk
 
--- edo.mk
+++ edo.mk
@@ -402,9 +402,8 @@ PRODUCT_PACKAGES += \
 TARGET_PROVIDES_POWERHAL := true
 
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
-    android.hardware.power@1.2.vendor \
-    libqti-perfd-client
+    libqti-perfd-client \
+    android.hardware.power-service.lineage-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -446,6 +445,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/sony \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/google/pixel \
     hardware/google/interfaces
