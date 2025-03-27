01a61671b3c28c26e449d053c6bb3b29ca1fc9c5
ahnet-69 <syedmusicrealise@gmail.com>
2025-03-27 09:08:51 +0900

    sm8250-common: rootdir: Move nfc config file handling back to init.sony.rc

--- rootdir/bin/init.sony.sh
+++ rootdir/bin/init.sony.sh
@@ -1,15 +1,17 @@
 #!/vendor/bin/sh
 
-model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
+model=`grep -aim1 'model:' /dev/block/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
 case "$model" in
     "XQ-AT42" | "XQ-AT52" | "XQ-AT72" | "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
         setprop vendor.radio.multisim.config dsds;;
     * )
-        setprop vendor.radio.multisim.config ss;;
+        setprop vendor.radio.multisim.config ssss;;
 esac
 
-# If the model string is empty, it must be a carrier model.
-if [ -z "$model" ]; then
-    setprop persist.vendor.nfc.config_file_name "libnfc-nxp-typef.conf"
+# If the model string is empty, it must be a ss JP model
+if [ "$model" == "" ]; then
+    setprop vendor.radio.ltalabel.model "JP"
+else
+    setprop vendor.radio.ltalabel.model "$model"
 fi
