e5a005de3ffbd9555d0fe3b91c5e227a2ffd0f71
ahnet-69 <syedmusicrealise@gmail.com>
2025-04-23 00:27:26 +0500

    Revert "sm8250-common: Build KProfiles"
    This reverts commit 81a56a302dc662a3adcee4d7fff54da7c623cc14.

--- edo.mk
+++ edo.mk
@@ -350,10 +350,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# KProfiles
-PRODUCT_PACKAGES += \
-    KProfiles
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
