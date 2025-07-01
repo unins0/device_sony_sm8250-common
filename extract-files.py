33308415cddd1c2a5dc11496eb3eb420446ac0a9
Cyber Knight <cyberknight755@gmail.com>
2025-07-02 00:01:09 +0500

    sm8250-common: Migrate to task_profiles for prebuilt init scripts
    - We ship custom task_profiles hence let's utilize it.
    
    Change-Id: I5351d33a825334fc37632e04f6541235def41d5a
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    [ahnet-69]: Also adapt to sony services

--- extract-files.py
+++ extract-files.py
@@ -109,6 +109,24 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/etc/msm_irqbalance.conf',
     ): blob_fixup()
         .regex_replace('IGNORED_IRQ=27,23,38$', 'IGNORED_IRQ=27,23,38,115,332'),
+    (
+        'vendor/etc/init/vendor.somc.hardware.camera.provider@1.0-service.rc',
+    ): blob_fixup()
+        .regex_replace(r'writepid.*', 'task_profiles CameraServiceCapacity MaxPerformance'),
+    (
+        'vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc',
+        'vendor/etc/init/vendor.qti.media.c2@1.0-service.rc'
+    ): blob_fixup()
+        .regex_replace(r'writepid.*', 'task_profiles ProcessCapacityHigh HighPerformance'),
+    (
+        'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc',
+    ): blob_fixup()
+        .regex_replace(r'writepid.*', 'task_profiles NNApiHALPerformance'),
+    (
+        'vendor/etc/init/vendor.semc.system.idd@1.0-service.rc',
+        'vendor/etc/init/init.sony.idd.rc',
+    ): blob_fixup()
+        .regex_replace(r'writepid.*', 'task_profiles ServiceCapacityLow'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
