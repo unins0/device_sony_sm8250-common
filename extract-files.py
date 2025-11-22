bd0a01e8012d444843db285e33046e8563af6dc2
realahnet <syedmusicrealise@gmail.com>
2025-11-22 22:52:33 +0500

    Revert "sm8250-common: proprietary: Nuke dup av libs"
    This reverts commit f5d77eabd1167d9bad4d717abfbfdef0dca1f8ed.

--- extract-files.py
+++ extract-files.py
@@ -26,7 +26,6 @@ namespace_imports = [
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
-    'vendor/qcom/common/system/av',
 ]
 
 
