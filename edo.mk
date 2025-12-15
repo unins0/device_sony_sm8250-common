630635d87eda7dc2903c20c857f2e2684d508b05
realahnet <syedmusicrealise@gmail.com>
2025-12-15 23:54:42 +0500

    Revert "sm8250-common: Adapt `thumbnail_block_model` flag to YAAP"
    This reverts commit 492ac0fb4a313f25fec3b63a0c71f97e1a8af849.

--- edo.mk
+++ edo.mk
@@ -299,7 +299,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
-$(call soong_config_set,stagefright,target_disables_thumbnail_block_model,true)
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
 
 # Light
 PRODUCT_PACKAGES += \
