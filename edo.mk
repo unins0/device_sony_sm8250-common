dee346e0701b35a7eab7dd0aaddcf733ffd5d97b
realahnet <syedmusicrealise@gmail.com>
2025-12-15 23:59:16 +0500

    sm8250-common: Adapt to voltage

--- edo.mk
+++ edo.mk
@@ -37,7 +37,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
