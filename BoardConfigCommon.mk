40e0cb83afc97a8df642470f915150b85b7b0e56
realahnet <syedmusicrealise@gmail.com>
2025-12-16 16:55:40 +0500

    [TMP]: Remove debugfs.config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,7 +76,6 @@ BOARD_DTB_OFFSET := 0x01f00000
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := \
-    vendor/debugfs.config \
     vendor/kona-perf_defconfig \
     vendor/edo.config
 
