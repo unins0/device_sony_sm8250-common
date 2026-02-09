914b24f6dd1fb6eaa6107be7bcc2fba15c269d97
unins0 <unins0000@proton.me>
2026-02-09 18:18:54 +0300

    Revert "sm8250-common: Switch system, system_ext, product partitions to erofs"
    This reverts commit 3e2bdfee96ad824984f76a68daf77e49a939e39d.

--- edo.mk
+++ edo.mk
@@ -103,7 +103,7 @@ PRODUCT_COPY_FILES += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
